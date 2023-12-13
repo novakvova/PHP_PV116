41250b7e396ac03f8ea57bc3d0479638d3ad1763
novakvova <novakvova@gmail.com>
2023-12-13 19:44:46 +0200

    Show categoires

--- pv116.rozetka.com/app/Http/Controllers/Api/CategoryController.php
+++ pv116.rozetka.com/app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categories;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function getList() {
+        $data = Categories::all();
+        return response()->json($data)
+            ->header("Content-Type", "application/json; charset=utf8");
+    }
 }
