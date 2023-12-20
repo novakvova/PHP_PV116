d6d7b4a9b594841dbc735b4d5a8dfcf6b4d903c8
novakvova <novakvova@gmail.com>
2023-12-20 20:53:37 +0200

    Begin working app

--- my-rozetka-app/src/components/home/HomePage.tsx
+++ my-rozetka-app/src/components/home/HomePage.tsx
@@ -1,8 +1,10 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
-import {Table} from "antd";
+import {Button, Popconfirm, Table} from "antd";
 import {ColumnsType} from "antd/es/table";
+import { DeleteOutlined } from '@ant-design/icons';
+
 
 interface ICategoryItem {
     id: number;
@@ -37,6 +39,31 @@ const HomePage = () => {
         {
             title: 'Назва',
             dataIndex: 'name'
+        },
+        {
+            title: 'Видалить',
+            dataIndex: 'delete',
+            render: (_, record) => (
+                <Popconfirm
+                    title="Are you sure to delete this category?"
+                    onConfirm={async () => {
+                        try {
+                            await axios.delete(`http://pv116.rozetka.com/api/categories/${record.id}`);
+                            setList(list.filter(x=>x.id!=record.id));
+
+                        } catch (error) {
+                            console.error('Error fetching category details:', error);
+                            throw error;
+                        }
+                    }}
+                    okText="Так"
+                    cancelText="Ні"
+                >
+                    <Button icon={<DeleteOutlined />}>
+                        Delete
+                    </Button>
+                </Popconfirm>
+            ),
         },
     ];
 
